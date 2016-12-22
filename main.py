f9cee398404e5b09ccfd947f078b1fcfb0362170
Simonas Viliunas <vilisimo@gmail.com>
2016-12-22 20:58:07 +0200

    Introduced argparse

---
+++ main.py
@@ -0,0 +1,5 @@
+import steamCLI.cli as console
+
+
+if __name__ == '__main__':
+    console.main()
--- steamCLI/cli.py
+++ steamCLI/cli.py
@@ -1,19 +1,26 @@
+import os
 import argparse
+
+from steamCLI.config import Config
+from steamCLI.steamapp import SteamApp
 
 
 def main():
+    config = Config('steamCLI', 'resources.ini')
+    APP_LIST = config.get_value('SteamAPIs', 'applist')
+
     parser = argparse.ArgumentParser(description="A command line application to retrieve information on Steam games.")
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("-t", "--title", action="store", metavar="",
-                       help="title of a game or an app on Steam")
+                       help="title of a game or an app on Steam", nargs="+")
     group.add_argument("-id", "--appID", type=int, action="store", metavar="",
                        help="id of a game or an app on Steam")
     args = parser.parse_args()
     if args.title:
-        print(args.title)
-    if args.appID:
-        print(args.appID * 2)
-
-
-if __name__ == '__main__':
-    main()
+        app_title = " ".join(args.title)  # In case contains multiple words.
+        print("Fetching id for '{}'...".format(app_title))
+        app = SteamApp(title=app_title)
+        app.assign_id(origin=APP_LIST)
+        print("{}'s id: {}".format(app.title, app.appid))
+    elif args.appID:
+        pass
