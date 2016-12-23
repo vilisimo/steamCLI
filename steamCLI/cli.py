bf039fa0183a0310363572434421896f5e437aff
vilisimo <vilisimo@gmail.com>
2016-12-23 02:00:51 +0200

    Input instead of nargs/store. Added unofficial api link to resources.

--- steamCLI/cli.py
+++ steamCLI/cli.py
@@ -1,6 +1,8 @@
 import os
 import argparse
 
+from string import capwords
+
 from steamCLI.config import Config
 from steamCLI.steamapp import SteamApp
 
@@ -9,18 +11,21 @@ def main():
     config = Config('steamCLI', 'resources.ini')
     APP_LIST = config.get_value('SteamAPIs', 'applist')
 
-    parser = argparse.ArgumentParser(description="A command line application to retrieve information on Steam games.")
+    parser = argparse.ArgumentParser(description="A CLI app to retrieve information about Steam apps.")
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-t", "--title", action="store", metavar="",
-                       help="title of a game or an app on Steam", nargs="+")
+    group.add_argument("-t", "--title", action="store_true",
+                       help="title of a game or an app on Steam")
     group.add_argument("-id", "--appID", type=int, action="store", metavar="",
                        help="id of a game or an app on Steam")
+    parser.add_argument("-d", "--description", action="store_true",
+                        help="include to see the app description")
     args = parser.parse_args()
     if args.title:
-        app_title = " ".join(args.title)  # In case contains multiple words.
-        print("Fetching id for '{}'...".format(app_title))
+        # Shell eats up special characters, such as ', &, etc. Hence, input().
+        app_title = input("Enter title:\t")
         app = SteamApp(title=app_title)
         app.assign_id(origin=APP_LIST)
-        print("{}'s id: {}".format(app.title, app.appid))
+        print("{} id: {}".format(capwords(app.title), 
+                                 app.appid if app.appid else "Not Found"))
     elif args.appID:
         pass
