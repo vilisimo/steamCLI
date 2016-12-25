d1373c00c286f31bcefc146d9c09f2cd48d9f963
vilisimo <vilisimo@gmail.com>
2016-12-25 15:04:26 +0200

    Further changes, experimenting with formatting, etc.

--- steamCLI/cli.py
+++ steamCLI/cli.py
@@ -1,40 +1,86 @@
 import argparse
 import pprint
 
-from string import capwords
-
 from steamCLI.config import Config
 from steamCLI.steamapp import SteamApp
 
-
-def main():
-    config = Config('steamCLI', 'resources.ini')
-    APP_LIST = config.get_value('SteamAPIs', 'applist')
+def create_parser(config):
+    """
+    Initializes parser with values from a config file.
+    """
 
     app_description = config.get_value('HelpText', 'app_help')
     parser = argparse.ArgumentParser(description=app_description)
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("-t", "--title", action="store_true",
                        help=config.get_value('HelpText', 'title_help'))
-    group.add_argument("-id", "--appID", type=int, action="store", metavar="",
-                       help=config.get_value('HelpText', 'id_help'))
+    group.add_argument("-id", "--appID", type=int, action="store",
+                       help=config.get_value('HelpText', 'id_help', ),
+                       metavar="val")
     parser.add_argument("-d", "--description", action="store_true",
                         help=config.get_value('HelpText', 'desc_help'))
-    args = parser.parse_args()
+    parser.add_argument("-r", "--region", action="store", metavar="val",
+                        help=config.get_value('HelpText', 'region_help'))
+    return parser.parse_args()
+
+def main():
+    config = Config('steamCLI', 'resources.ini')
+    APP_LIST = config.get_value('SteamAPIs', 'applist')
+    REGIONS = config.get_value('SteamRegions', 'regions')
+
+    args = create_parser(config)
+    if args.region and args.region not in REGIONS:
+        print("Region not recognized. Default will be used. Available values:\n"
+              "['au', 'br', 'ca', 'cn', 'eu1', 'eu2', 'ru', 'tr', 'uk', 'us']")
+        print()
+        args.region = None
 
     app = SteamApp()
     if args.title:
         # Shell eats up special characters, such as ', &, etc. Hence, input().
-        app_title = input("Enter title:\t")
+        app_title = None
+        while not app_title:
+            app_title = input("Enter title:\t").strip()
         app.title = app_title  # Reassign later, so that capwords are not needed
+
+        print("Gathering information, hold tight...\n")
         app.assign_id(origin=APP_LIST)
         # From here function should be used, as both appid and title will use it
-        print("{} id: {}".format(app.title,
-                                 app.appid if app.appid else "Not Found"))
         if app.appid:
-            json_data = app._fetch_json(
-                'http://store.steampowered.com/api/appdetails?appids={}'.format(app.appid))
-            pprint.pprint(json_data)
+            app.assign_json_info()
+            values = {
+                'title': app.title,
+                'release_date': app.release_date,
+                'meta': app.metacritic,
+                'description': app.description,
+                'initial': round(app.initial_price / 100, 2) if
+                app.initial_price else "N/A",
+                'current': round(app.final_price / 100, 2) if
+                app.final_price else "N/A",
+                'currency': app.currency if app.currency else "",
+                'discount': app.discount,
+            }
+
+            # Not the most efficient way, but more readable
+            title = "*** {title} ({release_date}) ***".format(**values)
+            print(title.center(79))
+            prices = "{current} {currency} ({discount}% of {initial} " \
+                     "{currency})".format(**values)
+            print(prices.center(79))
+            meta = "Metacritic score: {meta}".format(**values)
+            print(meta.center(79))
+            if args.description:
+                if len(values['description']) < 1:
+                    print("\nShort description unavailable.".center(79))
+                else:
+                    description = "\n{description}".format(**values)
+                    print(description.center(79))
+        else:
+            print("Application was not found. Make sure the title is correct!")
+
+        # json_data = app._fetch_json(
+        #     'http://store.steampowered.com/api/appdetails?appids={}'.format(app.appid))
+        # pprint.pprint(json_data)
 
     elif args.appID:
         app.appid = args.appID
