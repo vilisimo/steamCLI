9ae721cdb3b9dd755ef93986bf5d8a84c52000fd
vilisimo <vilisimo@gmail.com>
2017-01-31 20:33:25 +0200

    Add tests for website info formatting & type hints in Results class

--- steamCLI/results.py
+++ steamCLI/results.py
@@ -1,20 +1,69 @@
+from steamCLI.steamapp import SteamApp
+
+
 class Results:
-    def __init__(self, result=None, center=79):
+    def __init__(self, result: list=None, center: int=79):
         if result is None:
             self.result = []
         else:
             self.result = result
         self.center = center
 
-    def construct_steam_info(self, app):
+    def format_steam_info(self, app: SteamApp) -> str:
+        """
+        Formats information that was gathered from Steam API.
+
+        :param app: steam application from which info has to be formatted.
+        :return: formatted string.
+        """
+
         rel_date = app.release_date if app.release_date else 'no release date'
-        initial = round(app.initial / 100, 2) if app.initial else 'N/A'
-        current = round(app.current / 100, 2) if app.current else 'N/A'
+        initial = (round(app.initial_price / 100, 2) if app.initial_price
+                   else 'N/A')
+        current = (round(app.final_price / 100, 2) if app.final_price
+                   else 'N/A')
         currency = f' {app.currency}' if app.currency else ""
 
         self.result.append(f'*** {app.title} ({rel_date}) ***')
         self.result.append(f'{current}{currency} ({app.discount}% '
                            f'from {initial}{currency})')
-        self.result.append(f'Metacritic score: {app.meta}')
+        self.result.append(f'Metacritic score: {app.metacritic}')
 
-        return '\n'.join(line.center(self.center) for line in self.result)
+        return self._center_text(self.result)
+
+    def format_steam_website_info(self, app: SteamApp) -> str:
+        """
+        Formats information that was gathered from Steam website.
+
+        :param app: steam application from which info has to be formatted.
+        :return: formatted string.
+        """
+
+        self.result.append('\n')
+
+        if app.overall_count:
+            self.result.append(f'{app.overall_count} overall reviews '
+                               f'({app.overall_percent} positive)')
+        else:
+            self.result.append("No overall reviews available")
+
+        # It makes sense to show absence of recent reviews only if overall
+        # reviews are missing as well.
+        if app.overall_count and not app.recent_count:
+            self.result.append("No recent reviews available")
+
+        if app.recent_count:
+            self.result.append(f'{app.recent_count} recent reviews '
+                               f'({app.recent_percent} positive)')
+
+        return self._center_text(self.result)
+
+    def _center_text(self, text: list) -> str:
+        """
+        Helper method that centers given text.
+
+        :param text: list with string values that need to be centered.
+        :return: centered string.
+        """
+
+        return '\n'.join(line.center(self.center) for line in text)
--- test/test_results.py
+++ test/test_results.py
@@ -3,6 +3,7 @@ import unittest
 from unittest import mock
 
 from steamCLI.results import Results
+from steamCLI.steamapp import SteamApp
 
 
 class ResultTest(unittest.TestCase):
@@ -10,42 +11,41 @@ class ResultTest(unittest.TestCase):
 
     def setUp(self):
         self.results = Results()
+        self.app = SteamApp(config="placeholder")
 
     def test_steam_info_should_construct_centered_description(self):
         """ Ensures passing valid info constructs expected string. """
 
-        app = mock.Mock()
-        app.title = 'Borderlands'
-        app.release_date = '10 10 2010'
-        app.current = 1000
-        app.currency = 'GBP'
-        app.discount = '0'
-        app.initial = 1000
-        app.meta = '99'
+        self.app.title = 'Borderlands'
+        self.app.release_date = '10 10 2010'
+        self.app.final_price = 1000
+        self.app.currency = 'GBP'
+        self.app.discount = '0'
+        self.app.initial_price = 1000
+        self.app.metacritic = '99'
 
-        result = self.results.construct_steam_info(app)
+        result = self.results.format_steam_info(self.app)
 
         expected = list()
-        expected.append(f'*** {app.title} ({app.release_date}) ***')
-        expected.append(f'10.0 {app.currency} ({app.discount}% '
-                        f'from 10.0 {app.currency})')
-        expected.append(f'Metacritic score: {app.meta}')
+        expected.append(f'*** {self.app.title} ({self.app.release_date}) ***')
+        expected.append(f'10.0 {self.app.currency} ({self.app.discount}% '
+                        f'from 10.0 {self.app.currency})')
+        expected.append(f'Metacritic score: {self.app.metacritic}')
 
         self.assertEqual('\n'.join(ln.center(79) for ln in expected), result)
 
     def test_steam_info_without_valid_inputs(self):
         """ Ensure that null/empty values are correctly represented. """
 
-        app = mock.Mock()
-        app.title = 'Borderlands'
-        app.release_date = None
-        app.meta = None
-        app.current = None
-        app.initial = None
-        app.currency = None
-        app.discount = '0'
+        self.app.title = 'Borderlands'
+        self.app.release_date = None
+        self.app.metacritic = None
+        self.app.final_price = None
+        self.app.initial_price = None
+        self.app.currency = None
+        self.app.discount = '0'
 
-        result = self.results.construct_steam_info(app)
+        result = self.results.format_steam_info(self.app)
 
         expected = list()
         expected.append('*** Borderlands (no release date) ***')
@@ -53,4 +53,72 @@ class ResultTest(unittest.TestCase):
         expected.append('Metacritic score: None')
 
         self.assertEqual(result, '\n'.join(ln.center(79) for ln in expected))
+
+    def test_app_scores_no_missing_info(self):
+        """ Ensure that app scores are formatted correctly. """
+
+        self.app.overall_count = '1000'
+        self.app.overall_percent = '99%'
+        self.app.recent_count = '100'
+        self.app.recent_percent = '99%'
+        expected = list()
+        expected.append('\n')
+        expected.append('1000 overall reviews (99% positive)')
+        expected.append('100 recent reviews (99% positive)')
+        result = self.results.format_steam_website_info(self.app)
+
+        self.assertEqual(result, '\n'.join(ln.center(79) for ln in expected))
+
+    def test_app_scores_missing_all_info(self):
+        """ Ensure that app scores are reported missing. """
+
+        expected = list()
+        expected.append('\n')
+        expected.append("No overall reviews available")
+        result = self.results.format_steam_website_info(self.app)
+
+        self.assertEqual(result, '\n'.join(ln.center(79) for ln in expected))
+
+    def test_app_scores_recent_missing(self):
+        """ Ensure that recent app scores are reported missing. """
+
+        self.app.overall_count = '1000'
+        self.app.overall_percent = '99%'
+        expected = list()
+        expected.append('\n')
+        expected.append('1000 overall reviews (99% positive)')
+        expected.append('No recent reviews available')
+        result = self.results.format_steam_website_info(self.app)
+
+        self.assertEqual(result, '\n'.join(ln.center(79) for ln in expected))
+
+    def test_app_scores_overall_missing(self):
+        """ Ensure that overall app scores are reported missing. """
+
+        self.app.recent_count = '1000'
+        self.app.recent_percent = '99%'
+        expected = list()
+        expected.append('\n')
+        expected.append('No overall reviews available')
+        expected.append('1000 recent reviews (99% positive)')
+        result = self.results.format_steam_website_info(self.app)
+
+        self.assertEqual(result, '\n'.join(ln.center(79) for ln in expected))
+
+        # if args.scores:
+        #     print()
+        #     if not app.overall_count:
+        #         print("No reviews available".center(max_chars))
+        #     if app.overall_count:
+        #         overall_c = app.overall_count
+        #         overall_p = app.overall_percent
+        #         reviews = f"{overall_c} overall reviews ({overall_p} positive)"
+        #         print(reviews.center(max_chars))
+        #     if app.overall_count and not app.recent_count:
+        #         print("No recent reviews available".center(max_chars))
+        #     if app.recent_count:
+        #         recent_c = app.recent_count
+        #         recent_p = app.recent_percent
+        #         reviews = f"{recent_c} recent reviews ({recent_p} positive)"
+        #         print(reviews.center(max_chars))
 
