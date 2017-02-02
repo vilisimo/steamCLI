8a28955c407df65f8fbee5fb92b99d34c480aca6
vilisimo <vilisimo@gmail.com>
2017-02-02 21:39:22 +0200

    Add type annotations and improve docstrings

--- steamCLI/utils.py
+++ steamCLI/utils.py
@@ -10,22 +10,28 @@ TRANSFORMATIONS = {
     "9": 'ix',
 }
 
-WHITELIST = set('abcdefghijklmnopqrstuvwxy0123456789')
+ALLOWED = set('abcdefghijklmnopqrstuvwxy0123456789')
 
 
-def sanitize_title(title):
+def sanitize_title(title: str) -> str:
     """
-    Sanitizes input so that it doesn't can be submitted to Is There Any Deal.
+    Sanitizes input so that it doesn't can be submitted to Is There Any Deal:
+        1) digits need to be converted to their roman numeral counterparts
+        2) 'the' needs to be removed
+        3) special characters need to be removed
 
-    Note: for strings such as 'Астролорды: Оружие Пришельцев' it does not work, 
-    because ITAD encodes them in really strange fashion...
+    Note: for strings such as 'Астролорды: Оружие Пришельцев' it does not
+    work, because ITAD encodes them in really strange fashion... Common
+    libraries do not work as required, and there is no documentation of
+    encoding to help development of a custom tool.
+
+    :param title: app's title which needs to be sanitized
+    :return: sanitized string
     """
 
-    # remove_utf = title.lower().encode('ascii', 'ignore').decode("UTF-8")
-    # whitespaceless = remove_utf.replace(' ', '')
     lowercase_title = title.lower()
     articless = remove_articles(lowercase_title)
-    sanitized = ''.join(filter(WHITELIST.__contains__, articless))
+    sanitized = ''.join(filter(ALLOWED.__contains__, articless))
     numerals = list(sanitized)
 
     for index, key in enumerate(numerals):
@@ -36,12 +42,16 @@ def sanitize_title(title):
     return romanized
 
 
-def remove_articles(text):
+def remove_articles(text: str) -> str:
     """
     Removes articles from a given text. Not particularly fast, but since
     we are dealing with titles, no need to prematurely optimize.
 
     Note: currently, ITAD removes only 'the'...
+
+    :param text: string from which articles (currently only 'the') need to be
+                 removed
+    :return: formatted string
     """
 
     articles = {'the': ''}
@@ -49,17 +59,20 @@ def remove_articles(text):
     for word in text.split():
         if word not in articles:
             remaining.append(word)
-    # We could remove whitespaces, too. But then the function would be doing
-    # two things instead of one.
+
     return ' '.join(remaining)
 
 
-def calculate_discount(initial, current):
+def calculate_discount(initial: int, current: int) -> int:
     """
     Calculates the % difference between initial and current price.
 
     Note: when initial is 0 (that is, old price was lower than the new one -
     very unlikely in Steam), we assume that increase is (new price * 100)%.
+
+    :param initial: initial price
+    :param current: current price
+    :return integer: representing the discount
     """
 
     if initial is None or current is None:
