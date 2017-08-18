ec77db562b6989d022dfcdc7849613d0c6c5ece6
Simonas Viliunas <vilisimo@gmail.com>
2017-08-18 18:02:20 +0300

    Add setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='steamCLI',
+    version='0.0.1',
+    packages=['test', 'steamCLI'],
+    url='https://github.com/vilisimo/steamCLI',
+    license='MIT License',
+    author='vilisimo',
+    author_email='vilisimo@gmail.com',
+    description='Find relevant information about games on Steam straight from your command line.'
+)
