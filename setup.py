edfb83b97e2e3ab94b3d7deb427c981268b8f08c
Simonas Viliunas <vilisimo@gmail.com>
2017-08-19 22:05:19 +0300

    Update descriptions

--- setup.py
+++ setup.py
@@ -8,5 +8,10 @@ setup(
     license='MIT License',
     author='vilisimo',
     author_email='vilisimo@gmail.com',
-    description='Find relevant information about games on Steam straight from your command line.'
+    description='Find price information about games on Steam from the command line.',
+    long_description='The app provides a simple to use command line interface '
+                     'to check prices of  apps on Steam and compare them with '
+                     'historical lows. For that, https://isthereanydeal.com/ '
+                     'is used. To see a more in-depth and up-to-date description, '
+                     'please see the provided GitHub page.'
 )
